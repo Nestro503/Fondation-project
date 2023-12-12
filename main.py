66af2aa88b5bec9b7595a88e3740195144c44c60
Nestro503 <116881924+Nestro503@users.noreply.github.com>
2023-12-12 16:15:09 +0100

    capture video

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
+import numpy as np
+import cv2 as cv
 
-import cv2
+cap = cv.VideoCapture(1)   #pour choisir la source de capture video : 0 pour camera PC et 1 pour IV_Cam
 
-print(cv2.__version__)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv.imshow('frame', gray)
+
+    # Check if the user pressed 'q'
+    key = cv.waitKey(1)
+    if key == ord('q'):
+        break
+
+# When everything is done, release the capture
+cap.release()
+cv.destroyAllWindows()
